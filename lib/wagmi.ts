528cb405d4a9673f3e79d07316434d0bd0b39ec1
ndubuisi-ugwuja <ndujiovita1@gmail.com>
2026-09-12 03:28:20 +0100

    docs(wagmi): update app description to clarify asset recovery aspect

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -49,7 +49,7 @@ const alchemySepoliaRpcUrl = process.env.NEXT_PUBLIC_ALCHEMY_SEPOLIA_RPC_URL
 export const wagmiConfig = getDefaultConfig({
   appName:     'Aeternum',
   appDescription:
-    'A non-custodial, automated inheritance protocol for Ethereum assets.',
+    'A trustless, non-custodial inheritance and asset recovery protocol on Ethereum.',
   appUrl:      'https://www.aeternumvault.xyz',
   appIcon:     'https://www.aeternumvault.xyz/logo.png',
 
